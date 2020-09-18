9f4fcb49a173586e6b17a575c06f182f09f7e902
John Troutman <jtroutmanman@gmail.com>
2020-09-18 04:32:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   word_array = string.split(" ")
+  word_reverse = []
   word_array.each do |word|
-    word_reverse = []
     word_reverse << word.reverse
   end
   word_reverse.join(" ")
